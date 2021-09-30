8aacc01f713f62801f2e462e57aed1ecefd8fea4
piskunovandr <piskunov2000andrey@gmail.com>
2021-10-01 04:42:07 +0500

    Закончил main, остался адаптив и слайдеры

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(function(){
-
+	let input = document.querySelector('#name');
+	let label = document.querySelector('label[for="name"]');
+	input.addEventListener('focus', () => {
+	  label.style.visibility = 'hidden';
+	});
+	input.addEventListener('blur', () => {
+	  label.style.visibility = 'visible';
+	});
 })
